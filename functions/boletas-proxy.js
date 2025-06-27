e1651262a17c316d1e48bc03dbde460b02247a0e
Santiago Parra <PARRAPEREZ15@HOTMAIL.COM>
2025-06-27 15:02:44 -0500

    primer commit

---
+++ functions/boletas-proxy.js
@@ -0,0 +1,42 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+  // URL de tu Google Apps Script (¡REEMPLAZA CON TU URL REAL!)
+  const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwNnRcJAwj6M59tb7WQ2LtusGyOCF3tF-mJvT_oQpUAq-ApXqZzhsb8JPB-UCOxapyB/exec';
+  
+  try {
+    const options = {
+      method: event.httpMethod,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: event.body
+    };
+    
+    const response = await fetch(GOOGLE_SCRIPT_URL, options);
+    const data = await response.text();
+    
+    return {
+      statusCode: response.status,
+      body: data,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      }
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ 
+        status: 'error',
+        message: 'Error en el proxy: ' + error.message 
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+  }
+};
